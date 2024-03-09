c825007832fca8df8c1e6f536059c31c195773e7
ilpvc <2693285351@qq.com>
2024-03-09 10:19:56 +0800

    完成并测试person表的插入和新增

--- src/sql/sqlite/curd.rs
+++ src/sql/sqlite/curd.rs
@@ -1,16 +1,23 @@
+use std::env::VarError;
+use std::ptr::null;
 use rusqlite;
-use rusqlite::Error;
+use rusqlite::{Connection, Error};
 
 #[derive(Debug)]
-struct Person {
-    id: i32,
-    name: String,
-    age: i32
+pub struct Person {
+    pub id: i32,
+    pub name: String,
+    pub age: i32,
 }
 
-pub fn create_db() -> Result<rusqlite::Connection,Error>{
+pub fn create_connect() -> Connection {
     let db_file = "sqlite.db";
-    let connect = rusqlite::Connection::open(db_file)?;
+    let connection = Connection::open(db_file);
+    connection.unwrap()
+}
+
+pub fn create_table() -> Result<Connection, Error> {
+    let connect = create_connect();
     connect.execute(
         "CREATE TABLE IF NOT EXISTS person (
             id INTEGER PRIMARY KEY,
@@ -21,6 +28,35 @@ pub fn create_db() -> Result<rusqlite::Connection,Error>{
     )?;
     Ok(connect)
 }
+
+
+pub fn insert_person(person: Person) {
+    let connect = create_connect();
+    connect.execute(
+        "INSERT INTO person (name,age) values (?1,?2)",
+        [person.name, person.age.to_string()],
+    ).unwrap();
+}
+
+pub fn select_all_person() -> Result<Vec<Person>, Error> {
+    let connect = create_connect();
+    let mut stmt = connect.prepare(
+        "select * from person"
+    )?;
+    let person_list = stmt.query_map([], |row| {
+        Ok(Person {
+            id: row.get(0).unwrap(),
+            name: row.get(1).unwrap(),
+            age: row.get(2).unwrap(),
+        })
+    })?;
+    let mut persons: Vec<Person> = Vec::new();
+    for person in person_list {
+        persons.push(person.unwrap());
+    };
+    Ok(persons)
+}
+
 
 
 
--- src/test/test.rs
+++ src/test/test.rs
@@ -1,15 +1,44 @@
 #[cfg(test)]
 mod tests{
     use crate::sql::sqlite;
+    use crate::sql::sqlite::curd::select_all_person;
+
     #[test]
     fn test_sqlite(){
-        let connection = sqlite::curd::create_db();
+        let connection = sqlite::curd::create_table();
         match connection {
             Ok(_connect)=>{
                 println!("创建成功！:")
             },
             Err(sqlite)=>{
                 println!("创建失败！：{}",sqlite)
+            }
+        }
+    }
+    #[test]
+    fn test_sqlite_insert(){
+        let name = String::from("colzry");
+        let age = 18;
+
+        let person = sqlite::curd::Person{
+            id:0,
+            name,
+            age,
+        };
+        sqlite::curd::insert_person(person)
+    }
+
+    #[test]
+    fn test_select_person(){
+        let result = select_all_person();
+        match result {
+            Ok(persons)=>{
+                for person in persons {
+                    println!("person name:{}",person.name)
+                }
+            },
+            Err(sqlite)=>{
+                println!("err:{}",sqlite)
             }
         }
     }
