a8b3deb43b02e57f9d0739088a3747abe713e748
ilpvc <2693285351@qq.com>
2024-03-14 22:28:39 +0800

    完成16进制解码

--- src/codec/base16_codec.rs
+++ src/codec/base16_codec.rs
@@ -14,3 +14,30 @@ pub fn encode(data: &[u8]) -> Vec<char>{
     }
     out
 }
+
+pub fn decode(data: &[char]) -> Option<Vec<u8>>{
+    if data.len()%2==1 {
+        return None
+    }
+    let mut de_data = Vec::with_capacity(data.len()/2);
+    for chunk in data.chunks(2) {
+        let high_nibble = match chunk[0] {
+            '0'..='9' => chunk[0] as u8 - b'0',
+            'A'..='F' => chunk[0] as u8 - b'A' + 10,
+            'a'..='f' => chunk[0] as u8 - b'a' + 10,
+            _ => return None, // 如果遇到非十六进制字符，则无法进行解码
+        };
+
+        let low_nibble = match chunk[1] {
+            '0'..='9' => chunk[1] as u8 - b'0',
+            'A'..='F' => chunk[1] as u8 - b'A' + 10,
+            'a'..='f' => chunk[1] as u8 - b'a' + 10,
+            _ => return None, // 如果遇到非十六进制字符，则无法进行解码
+        };
+
+        let byte = (high_nibble << 4) | low_nibble;
+
+        de_data.push(byte)
+    }
+    Some(de_data)
+}
--- src/codec/test.rs
+++ src/codec/test.rs
@@ -1,13 +1,19 @@
 #[cfg(test)]
 mod tests {
-    use crate::codec::base16_codec::encode;
+    use crate::codec::base16_codec::{decode, encode};
 
     #[test]
     fn test1(){
-        let percent = "%%%%你好";
+        let percent = "*&^";
         let vec = encode(percent.as_bytes());
         vec.iter().for_each(|e|{
             print!("{}",e)
-        })
+        });
+        println!("\n");
+        let option = decode(vec.as_slice());
+        match option {
+            Some(v)=>v.iter().for_each(|x|print!("{}",x)),
+            None=>print!("null")
+        }
     }
 }
